ebdfb613d8e21bc44acddf91a209df110a9ed010
ConstantineGle <constantine.glebov@gmail.com>
2023-06-08 12:51:59 +0700

    Initial commit

---
+++ src/main/java/ru/netology/springbootdemo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package ru.netology.springbootdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${hello.from:undefined}")
+
+    private String from;
+
+    @GetMapping("/")
+    private String hello() {
+        return String.format("Hello from %s", from);
+    }
+}
